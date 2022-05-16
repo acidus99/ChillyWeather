c5fae2f42c8542292e0d76b9259dfe2b53cdef90
Acidus <acidus99@users.noreply.github.com>
2022-05-16 10:52:57 -0400

    Adding IsSunUp functions

--- Models/Forecast.cs
+++ Models/Forecast.cs
@@ -36,6 +36,9 @@ namespace Chilly.Models
         public float HighTemp { get; set; }
         public float LowTemp { get; set; }
 
+        public DateTime Sunrise { get; set; }
+        public DateTime Sunset { get; set; }
+
         /// <summary>
         /// 0-1.0: % chance of Precipitation
         /// </summary>
@@ -44,16 +47,41 @@ namespace Chilly.Models
 
     public class Forecast
     {
-        public GeoLocale Location;
+        public GeoLocale Location { get; set; }
 
-        public CurrentCondition Current;
+        public CurrentCondition Current { get; set; }
 
-        public HourlyCondition [] Hourly;
+        public HourlyCondition [] Hourly { get; set; }
 
-        public DailyCondition[] Daily;
+        public DailyCondition[] Daily { get; set; }
 
-        public bool IsMetric;
+        public bool IsMetric { get; set; }
 
+        /// <summary>
+        /// Determines if a datetime occuring when the sun is up. If we don't
+        /// have sunrise data for it, return true
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public bool IsSunUp(DateTime time)
+        {
+            foreach(DailyCondition daily in Daily)
+            {
+                if(daily.Time.Date == time.Date)
+                {
+                    if(time < daily.Sunrise || time > daily.Sunset)
+                    {
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            //if we get here, something is wrong with the forecast, so default to true
+            return true;
+        }
+
+        public bool IsSunCurrentlyUp
+            => IsSunUp(Current.Time);
     }
 
     public enum WeatherType
