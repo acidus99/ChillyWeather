d444149c6cd3d7b75ab2798d1d354e94375658f7
Acidus <acidus99@users.noreply.github.com>
2022-05-30 09:45:30 -0400

    use new function

--- Renderer.cs
+++ Renderer.cs
@@ -19,7 +19,7 @@ namespace Chilly
 
         public void Render(Forecast forecast)
         {
-            Fout.WriteLine($"Weather for {forecast.Location.Name} @ {formatter.FormatTime(forecast.Current.Time)}");
+            Fout.WriteLine($"Weather for {forecast.Location.Name} @ {formatter.FormatDayTime(forecast.Current.Time)}");
             Fout.WriteLine($"{formatter.EmojiForWeather(forecast.Current.Weather.Type, forecast.IsSunCurrentlyUp)} {formatter.FormatTemp(forecast.Current.Temp)}");
             Fout.WriteLine($"{formatter.FormatDescription(forecast.Current.Weather)}");
 
