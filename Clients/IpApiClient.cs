e0c22636b910a1806048420f7b773ad205a767b2
Acidus <acidus99@users.noreply.github.com>
2022-08-03 19:15:23 -0400

    adding additional IP to location service

---
+++ Clients/IpApiClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+
+using Chilly.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Chilly.Clients
+{
+    /// <summary>
+    /// Gets a locale for an IP via http://ip-api.com/json/
+    /// </summary>
+    public class IpApiClient : ILocaleClient
+    {
+        WebClient client = new WebClient();
+        public GeoLocale GetIPLocale(string ip)
+        {
+            var url = $"http://ip-api.com/json/{ip}";
+
+            var json = client.DownloadString(url);
+            var resp = JObject.Parse(json);
+            return new GeoLocale
+            {
+                Name = Cleanse(resp["city"]),
+                State = Cleanse(resp["regionName"]),
+                Country = Cleanse(resp["countryCode"]),
+                Longitude = Coordinate(resp["lon"]),
+                Latitude = Coordinate(resp["lat"])
+            };
+        }
+
+        public GeoLocale GetCurrentLocale()
+            => GetIPLocale("");
+
+        private string Cleanse(JToken token)
+            => token?.ToString() ?? "";
+
+        private double Coordinate(JToken token)
+            => (token != null) ? Convert.ToDouble(token.ToString()) : 0;
+    }
+}
