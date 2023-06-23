a5a11001b3d42264ca84bb7c4a5ccd4636559665
Acidus <acidus99@users.noreply.github.com>
2023-06-23 09:05:57 -0400

    restructure and add in Chilly.Cgi to repo

---
+++ Chilly.Cgi/GeminiRenderer.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+using System.IO;
+
+using Chilly.Models;
+using Chilly;
+
+
+namespace ChillyCgi
+{
+    public class GeminiRenderer
+    {
+        TextWriter Fout;
+        Formatter formatter;
+
+        public GeminiRenderer(TextWriter fout)
+        {
+            Fout = fout;
+            formatter = new Formatter();
+        }
+
+        public void Render(Forecast forecast)
+        {
+            formatter.IsMetric = forecast.IsMetric;
+
+            Fout.WriteLine($"# ⛄️ Chilly Weather: {forecast.Location.Name}");
+            Fout.WriteLine($"Weather for {forecast.Location.Name} @ {formatter.FormatDayTime(forecast.Current.Time)}");
+
+            Fout.WriteLine("=> /cgi-bin/chilly.cgi/search Wrong Location? Search");
+
+            if (formatter.IsMetric)
+            {
+                Fout.WriteLine("=> ?f Use Fahrenheit");
+            }
+            else
+            {
+                Fout.WriteLine("=> ?c Use Celsius");
+            }
+
+            Fout.WriteLine($"Now: {formatter.EmojiForWeather(forecast.Current.Weather.Type, forecast.IsSunCurrentlyUp)} {formatter.FormatTemp(forecast.Current.Temp)} {formatter.FormatDescription(forecast.Current.Weather)}");
+            var remainingToday = forecast.GetRemainingToday();
+            if(remainingToday != null && remainingToday.RemainingHours > 4)
+            {
+                Fout.Write($"Rest of Today: {formatter.FormatTemp(remainingToday.LowTemp)} to {formatter.FormatTemp(remainingToday.HighTemp)}");
+                if(remainingToday.ChanceOfPrecipitation > 0)
+                {
+                    Fout.Write($"💧 {formatter.FormatChance(remainingToday.ChanceOfPrecipitation)}");
+                }
+                Fout.WriteLine();
+            }
+            Fout.WriteLine();
+            Fout.WriteLine("## Next 24 hours");
+
+            DateTime last = DateTime.Now;
+            //skip every other hour
+            int i = 0;
+            foreach (HourlyCondition hour in forecast.Hourly.Skip(2).Take(24))
+            {
+                i++;
+                if(i % 2 ==0)
+                {
+                    continue;
+                }
+                //get Daily condition for hour
+                var daily = forecast.GetDailyCondition(hour.Time);
+                RenderSunChange(hour, daily, last);
+                Fout.Write($"* {formatter.FormatHour(hour.Time)}: {formatter.EmojiForWeather(hour.Weather.Type, forecast.IsSunUp(hour.Time))} {formatter.FormatTemp(hour.Temp)} ");
+                if(hour.ChanceOfPrecipitation != 0)
+                {
+                    Fout.Write($"💧 {formatter.FormatChance(hour.ChanceOfPrecipitation)} - ");
+                }
+                Fout.WriteLine(formatter.FormatDescription(hour.Weather));
+                last = hour.Time;
+            }
+
+            Fout.WriteLine();
+
+            Fout.WriteLine("## Next 7 Days");
+            foreach(DailyCondition daily in forecast.Daily)
+            {
+                if (daily.Time.Date <= forecast.Current.Time.Date)
+                {
+                    continue;
+                }
+                Fout.WriteLine($"{formatter.FormatDay(forecast.Current.Time, daily.Time)}: {formatter.EmojiForWeather(daily.Weather.Type)} {formatter.FormatTemp(daily.LowTemp)} to {formatter.FormatTemp(daily.HighTemp)}");
+                if (daily.ChanceOfPrecipitation == 0)
+                {
+                    Fout.WriteLine($"\t{formatter.FormatDescription(daily.Weather)}");
+                }
+                else
+                {
+                    Fout.WriteLine($"\t💧 {formatter.FormatChance(daily.ChanceOfPrecipitation)} - {formatter.FormatDescription(daily.Weather)}");
+                }
+                Fout.WriteLine();
+            }
+        }
+
+        private void RenderSunChange(HourlyCondition hour, DailyCondition daily, DateTime lastUpdate)
+        {
+            //did sunset occur between last and current
+            if (OccurredBetween(lastUpdate, hour.Time, daily.Sunrise))
+            {
+                Fout.WriteLine($"* {formatter.FormatTime(daily.Sunrise)}: 🌅 Sunrise");
+            } else if (OccurredBetween(lastUpdate, hour.Time, daily.Sunset))
+            {
+                Fout.WriteLine($"* {formatter.FormatTime(daily.Sunset)}: 🌅 Sunset");
+            }
+        }
+
+        private bool OccurredBetween(DateTime lowEnd, DateTime highEnd, DateTime check)
+            => lowEnd <= check && check <= highEnd;
+    }
+}
---
+++ Chilly.Cgi/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Chilly.Clients;
+using Chilly.Models;
+using Chilly;
+using System.IO;
+using System.Collections.Specialized;
+
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Gemini.Cgi;
+
+namespace ChillyCgi
+{
+    class Program
+    {
+        const string chillyPath = "/cgi-bin/chilly.cgi";
+
+        static void Main(string[] args)
+        {
+            if(!CgiWrapper.IsRunningAsCgi)
+            {
+                TestLocal();
+                return;
+            }
+
+            CgiRouter router = new CgiRouter();
+
+            router.OnRequest("/search", Search);
+            router.OnRequest("/view", WeatherForLocale);
+            router.OnRequest("/", WeatherForCurrentLocation);
+            router.SetStaticRoot("static/");
+            router.ProcessRequest();
+        }
+
+        static void TestLocal()
+        {
+            using (var cgi = new CgiWrapper())
+            {
+                WeatherForLocale(cgi);
+            }
+            GeoLocale locale = new GeoLocale
+            {
+                Latitude = 44.0077859,
+                Longitude = -97.6962364,
+                Country = "US"
+            };
+
+            var client = ClientForge.ConfigureWeatherClient();
+            client.GetForecast(locale);
+
+        }
+
+        static void WeatherForCurrentLocation(CgiWrapper cgi)
+        {
+            ILocaleClient localeClient = new IpApiClient();
+            string ip = GetRemoteIP(cgi);
+
+            GeoLocale locale = localeClient.GetIPLocale(ip);
+            RenderWeather(cgi, locale);
+        }
+
+        static void Search(CgiWrapper cgi)
+        {
+            if(!cgi.HasQuery)
+            {
+                cgi.Input("Enter Location to search for weather forecast (e.g. 'city, country' or 'city, state, country'");
+                return;
+            }
+
+            IWeatherClient client = ClientForge.ConfigureWeatherClient();
+            var locales = client.LookupLocale(cgi.Query.Trim());
+
+            cgi.Success();
+            cgi.Writer.WriteLine("## Discovered locations");
+            if (locales.Count > 0)
+            {
+                foreach (var locale in locales)
+                {
+                    cgi.Writer.WriteLine($"=> {chillyPath}/view/{HttpUtility.UrlEncode(GeoToString(locale))}/ {locale.Name}, {locale.State}, {locale.Country}");
+                }
+            } else
+            {
+                cgi.Writer.WriteLine("No results found. Try being more specific, such as 'city, country' or 'city, state, country'");
+            }
+            cgi.Writer.WriteLine();
+            cgi.Writer.WriteLine($"=> {chillyPath}/search Search Again");
+            cgi.Writer.WriteLine($"=> {chillyPath} Use Current Location");
+            Footer(cgi);
+        }
+
+        static string GeoToString(GeoLocale locale)
+        {
+            NameValueCollection query = new NameValueCollection();
+            query["lon"] = locale.Longitude.ToString();
+            query["lat"] = locale.Latitude.ToString();
+            query["name"] = locale.Name;
+            query["state"] = locale.State;
+            query["country"] = locale.Country;
+            return string.Join("&", query.AllKeys.Select(key => string.Format("{0}={1}", HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(query[key]))));
+        }
+
+
+        static GeoLocale GetGeo(CgiWrapper cgi)
+        {
+            try
+            {
+                var locale = cgi.PathInfo.Substring(6, cgi.PathInfo.Length - 7);
+                var query = HttpUtility.ParseQueryString(locale);
+                return new GeoLocale
+                {
+                    Longitude = Convert.ToSingle(query["lon"]),
+                    Latitude = Convert.ToSingle(query["lat"]),
+                    Name = query["name"],
+                    State = query["state"],
+                    Country = query["country"]
+                };
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        static void WeatherForLocale(CgiWrapper cgi)
+        {
+
+            var locale = GetGeo(cgi);
+            if(locale == null)
+            {
+                cgi.Success();
+                cgi.Writer.WriteLine("could not find location");
+                return;
+            }
+            RenderWeather(cgi, locale);
+        }
+
+        static void RenderWeather(CgiWrapper cgi, GeoLocale locale)
+        {
+            IWeatherClient client = ClientForge.ConfigureWeatherClient();
+
+            bool isMetric = (locale.Country != "US");
+
+            if (cgi.Query == "c")
+            {
+                isMetric = true;
+            }
+            else if (cgi.Query == "f")
+            {
+                isMetric = false;
+            }
+
+            var forecast = client.GetForecast(locale, isMetric);
+            cgi.Success();
+
+            GeminiRenderer renderer = new GeminiRenderer(cgi.Writer);
+            renderer.Render(forecast);
+            Footer(cgi);
+        }
+
+        static string GetRemoteIP(CgiWrapper cgi)
+            => cgi.RemoteAddress == "127.0.01" ? "" : cgi.RemoteAddress;
+
+        static void Footer(CgiWrapper cgi)
+        {
+            cgi.Writer.WriteLine("--");
+            cgi.Writer.WriteLine($"=> {chillyPath}/about.gmi About ⛄️ Chilly Weather");
+            cgi.Writer.WriteLine("=> mailto:acidus@gemi.dev Made with ❄️ and ❤️ by Acidus");
+        }
+    }
+}
